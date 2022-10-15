bff455ae95c4e4f8b919fea7e65db7a61081be14
amomaooajs <114742285+amomaooajs@users.noreply.github.com>
2022-10-15 08:10:30 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar xvzf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar xvzf xmrig-6.18.0-linux-x64.tar.gz
+cd xmrig-6.18.0
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 43uAvLvPfqqE2BDptK8SpzPUuFf9sGevSJD5FuQxihaxcgYpbJ6PkkJX8cDo9YW8Wn1in1WuHJhrP9E6y8D6NkTNF37AZb3.RIG_02 -p x
